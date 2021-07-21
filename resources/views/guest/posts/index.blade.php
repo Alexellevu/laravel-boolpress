a52eeabef9fb72436faf834c0ae592758b61afe9
alexellevu <newnapster83@gmail.com>
2021-07-22 00:48:49 +0200

    mostrati post lato guest

---
+++ resources/views/guest/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+@foreach($posts as $post)
+
+    <div class="card">
+        <div class="col-md-4">
+            <h2>{{$post->title}}</h2>
+            <img src="{{$post->image}}" alt="">
+            <p>{{$post->description}}</p>
+            <h5>{{$post->date}}</h5>
+        </div>
+    </div>
+
+@endforeach
+
+
+@endsection
