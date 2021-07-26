3d312d0ad18d8478978a096e69d5641ec6d43da6
alexellevu <newnapster83@gmail.com>
2021-07-26 16:40:40 +0200

    risolto problema sull index del guest per il singolo post

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -6,24 +6,23 @@
 
 <div class="container">
     <div class="row">
-    @foreach($posts as $post)
+    
 
         <div class="col-md-4">
             <div class="card">
-                    <img class="card-img-top" src="{{$post->image}}" alt="">
+                <img class="card-img-top" src="{{$post->image}}" alt="">
                 <div class="card-body">
-                    <h4 class="card-title">{{$post->title}}</h4>
-                    <p class="card-text">{{$post->description}}</p>
-                </div>
+                <h4 class="card-title">{{$post->title}}</h4>
+                <p class="card-text">{{$post->description}}</p>
             </div>
         </div>
-
-       
-
-    @endforeach 
-
     </div>
-</div>
+
+
+
+
+
+
 
 
 @endsection
