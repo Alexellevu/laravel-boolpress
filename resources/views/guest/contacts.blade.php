ebf21e4da4ac158164dc39ca1c9b0d865203c00d
alexellevu <newnapster83@gmail.com>
2021-08-02 18:07:36 +0200

    inserita conferma mail nella pagina contacts con if

--- resources/views/guest/contacts.blade.php
+++ resources/views/guest/contacts.blade.php
@@ -16,6 +16,19 @@
   </div>
 @endif
 
+@if(session('message'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <strong>{{session('message')}}</strong> 
+    </div>
+    
+    <script>
+      $(".alert").alert();
+    </script>
+@endif
+
 <form action="{{route('contacts.send')}}" method="post" class="m-5">
     @csrf
     <div class="form-group">
